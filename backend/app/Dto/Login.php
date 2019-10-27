39bedf07efe2ea245a4414a8c255ec59a34c5946
Dusan Malusev <dusan.998@outlook.com>
2019-10-27 18:30:32 +0100

    Implementing dashboard

--- backend/app/Dto/Login.php
+++ backend/app/Dto/Login.php
@@ -3,7 +3,13 @@
 
 namespace App\Dto;
 
-
+/**
+ * Class Login
+ *
+ * @package App\Dto
+ * @property string email
+ * @property string password
+ */
 class Login extends Base
 {
     protected $email;
