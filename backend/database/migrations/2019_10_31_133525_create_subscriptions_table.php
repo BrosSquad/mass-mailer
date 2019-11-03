9fcf17e4247f35c4fc1b88d0acbbfdfb9f975bd9
Dusan Malusev <dusan.998@outlook.com>
2019-11-03 09:47:31 +0100

    Fixing the migrations

--- backend/database/migrations/2019_10_31_133525_create_subscriptions_table.php
+++ backend/database/migrations/2019_10_31_133525_create_subscriptions_table.php
@@ -11,26 +11,23 @@ class CreateSubscriptionsTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('subscriptions', static function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 50);
             $table->string('surname', 50);
             $table->string('email', 255)->unique();
-            $table->unsignedInteger('application_id');
-            
-            
             $table->timestamps();
         });
     }
-    
+
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('subscriptions');
     }
